35ee1528d476b9476386ca53508300a5ed9f3ad1
Allie <alliecaton3@gmail.com>
2020-12-24 14:53:54 -0500

    commit tree cd6325e85dbb6d92034d1915ab6b982ec22b310e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,44 @@
+require 'pry'
 class Triangle
-  # write code here
+
+  attr_accessor :equlateral, :isosceles, :scalene
+
+  def initialize(x, y, z)
+    @x = x
+    @y = y 
+    @z = z
+  end
+
+  def kind
+    is_triangle? 
+
+    triangle= [@x, @y, @z].sort
+    x = triangle[0]
+    y = triangle[1]
+    z = triangle[2]
+
+    if (x == y && z) && (x && y && z > 0)
+      :equilateral
+    elsif (x == y && x > z) || (y == z && y > x)
+      :isosceles
+    elsif x != y && z
+      :scalene  
+    end
+  end
+
+  def is_triangle?
+    if @x <= 0 && @y <= 0 && @z <= 0 
+      raise TriangleError
+    elsif @x + @y <= @z || @y + @z <= @x || @z + @x <= @y 
+      raise TriangleError
+    else 
+      true
+    end 
+  end
+
+
+  class TriangleError < StandardError
+  end
+
+
 end
